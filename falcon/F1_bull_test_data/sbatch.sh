5a334f52b6cdc4022cf45290496be4cdbc4e89a6
Audrey Stott <astott@zeus-1.pawsey.org.au>
2020-02-21 09:11:33 +0800

    added sbatch script

---
+++ falcon/F1_bull_test_data/sbatch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#SBATCH --account=pawsey0002
+#SBATCH --partition=workq
+#SBATCH --nodes=2
+#SBATCH --cpus-per-task=28
+
+module load singularity
+
+srun singularity exec pb-assembly_0.0.8--0.sif fc_run fc_run.cfg 

55a8a1b521b65f6d441303dac3596558e65b3ba7
audreystott <audreystott@outlook.com>
2020-03-04 11:39:33 +0800

    added mv .log filenames

--- falcon/F1_bull_test_data/sbatch_phase.sh
+++ falcon/F1_bull_test_data/sbatch_phase.sh
@@ -7,4 +7,7 @@
 
 module load singularity
 
+// to ensure log file from fc_unzip is not overwritten by this step
+mv all.log all1.log
+
 srun singularity exec pb-assembly_0.0.8--0.sif fc_phase.py fc_phase.cfg &> run2.std
--- falcon/F1_bull_test_data/sbatch_unzip.sh
+++ falcon/F1_bull_test_data/sbatch_unzip.sh
@@ -7,4 +7,7 @@
 
 module load singularity
 
+// to ensure log file from fc_run is not overwritten by this step
+mv all.log all0.log
+
 srun singularity exec pb-assembly_0.0.8--0.sif fc_unzip.py fc_unzip.cfg &> run1.std
